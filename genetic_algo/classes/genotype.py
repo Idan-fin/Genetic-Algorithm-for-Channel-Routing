dc21f155b8feb92e4ee6f2773a211cbef0e030c8
yoto5 <yotam.toledano@gmail.com>
2022-08-14 17:11:08 +0300

    - random routing first draft - a lot of backtracking - before testing

--- genetic_algo/classes/genotype.py
+++ genetic_algo/classes/genotype.py
@@ -4,6 +4,8 @@ from typing import List
 class Genotype:
 
     def __init__(self, num_of_row: int, pins_position: List[List[int]]):
+
+        # TODO: decide if grid should be List[List[List[int]]] or List[List[List[Nucleotide]]]
         self.grid = self._generate_initial_genotype(num_of_row=num_of_row, pins_position=pins_position)
 
     def _generate_initial_genotype(self, num_of_row: int, pins_position: List[List[int]]) -> List[List[List[int]]]:
