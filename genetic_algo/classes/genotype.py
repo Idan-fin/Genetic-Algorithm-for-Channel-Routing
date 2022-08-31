acf10f08e79bf70a4ff7b2db9eb78e7ee8c56c83
Idan-fin <53435486+Idan-fin@users.noreply.github.com>
2022-08-31 18:49:17 +0300

    create graph from genotype(untested)

--- genetic_algo/classes/genotype.py
+++ genetic_algo/classes/genotype.py
@@ -1,4 +1,5 @@
 from typing import List
+from igraph import *
 
 
 class Genotype:
@@ -8,3 +9,34 @@ class Genotype:
 
     def _generate_initial_genotype(self, num_of_row: int, pins_position: List[List[int]]) -> List[List[List[int]]]:
         pass
+
+    def _calculate_edge_index(self, x: int, y: int, z: int) -> int:
+        return len(self.grid)*len(self.grid[0])*z+len(self.grid)*y+x
+        pass
+
+    def create_graph(self, net_id: int) -> Graph:
+        g = Graph()
+        g.add_vertices(len(self.grid)*len(self.grid[0])*len(self.grid[0][0]))
+        for i, y in enumerate(self.grid):
+            for j, z in enumerate(y):
+                for k, val in enumerate(z):
+                    if i < len(self.grid)-2 and abs(self.grid[i+1][j][k]) == abs(val) == net_id:
+                        g.add_edge(self._calculate_edge_index(i, j, k), (self._calculate_edge_index(i+1, j, k)))
+                        pass
+                    if j < len(y)-2 and i > 0 and abs(self.grid[i][j+1][k]) == abs(val) == net_id:
+                        g.add_edge(self._calculate_edge_index(i, j, k), (self._calculate_edge_index(i, j+1, k)))
+                        pass
+                    if k == 0 and abs(self.grid[i][j][k+1]) == abs(val) == net_id:
+                        g.add_edge(self._calculate_edge_index(i, j, k), (self._calculate_edge_index(i, j, k+1)))
+                        pass
+        return g
+        pass
+
+    #def calculate_genotype_index(self, index: int) -> (x, y, z):
+
+    #def (self, )
+
+
+
+
+
