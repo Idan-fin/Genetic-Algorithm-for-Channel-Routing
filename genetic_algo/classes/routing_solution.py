fd16f7e6e8beb220f873a26f5c3a64c38fa9a1ff
yoto5 <yotam.toledano@gmail.com>
2022-09-01 16:59:04 +0300

    - fix random routing to work with genotype - fix vertival/horizontal lines functions - use find path method

--- genetic_algo/classes/routing_solution.py
+++ genetic_algo/classes/routing_solution.py
@@ -39,7 +39,8 @@ class RoutingSolution:
     def calc_fitness(self) -> float:
         pass
 
-    def _choose_layer(self) -> int:
+    @staticmethod
+    def _choose_layer() -> int:
 
         # TODO: check about the preferred routing direction of each layer.
         # see section 4.3 in the article abot choosing a layer
@@ -50,111 +51,111 @@ class RoutingSolution:
     def _horizontal_line(self,
                          starting_point: Point2D,
                          net_number: int,
-                         grid: List[list[List[int]]]) -> Point2D:
+                         genotype: Genotype) -> Point2D:
 
-        number_of_columns = len(grid[0][0])
         left_x, right_x = starting_point.x, starting_point.x
         layer = self._choose_layer()
 
-        # rout from starting point left
+        # draw line from starting point left
         while left_x >= 0:
-            current_node_val = grid[layer][starting_point.y][left_x]
+            current_node_val = genotype.grid[layer][starting_point.y][left_x]
 
             if abs(current_node_val) > 0:
                 break
-            grid[layer][starting_point.y][left_x] = net_number
+            genotype.grid[layer][starting_point.y][left_x] = net_number
             left_x -= 1
+        left_x = 0 if left_x < 0 else left_x
 
-        # rout from starting point right
-        while right_x < number_of_columns:
-            current_node_val = grid[layer][starting_point.y][right_x]
+        # draw line from starting point right
+        while right_x < genotype.num_of_columns:
+            current_node_val = genotype.grid[layer][starting_point.y][right_x]
 
             if abs(current_node_val) > 0:
                 break
-            grid[layer][starting_point.y][right_x] = net_number
+            genotype.grid[layer][starting_point.y][right_x] = net_number
             right_x += 1
+        right_x = genotype.num_of_columns if right_x >= genotype.num_of_columns else right_x + 1
 
-        return Point2D(randrange(left_x + 1, right_x - 1), y=starting_point.y)
+        return Point2D(x=randrange(left_x, right_x), y=starting_point.y)
 
     def _vertical_line(self,
                        starting_point: Point2D,
                        net_number: int,
-                       grid: List[list[List[int]]],
+                       genotype: Genotype,
                        initial_line: bool = False) -> Point2D:
 
-        number_of_rows = len(grid[0])
         top_y, bottom_y = starting_point.y, starting_point.y
         layer = self._choose_layer()
 
-        # rout from starting point down
+        # draw line from starting point down
         while bottom_y >= 0:
-            current_node_val = grid[layer][bottom_y][starting_point.x]
+            current_node_val = genotype.grid[layer][bottom_y][starting_point.x]
 
             # see example for this condition in the article section 4.3, image (a).
-            if abs(current_node_val) > 0 and (abs(current_node_val) != net_number or not initial_line):
+            # initial line don't stop on nodes with the same net number
+            if abs(current_node_val) > 0 and (abs(current_node_val) != net_number and not initial_line):
                 break
-            grid[layer][bottom_y][starting_point.x] = net_number
+            genotype.grid[layer][bottom_y][starting_point.x] = net_number
             bottom_y -= 1
+        bottom_y = 0 if bottom_y < 0 else bottom_y
 
-        # rout from starting point up
-        while top_y < number_of_rows:
-            current_node_val = grid[layer][top_y][starting_point.x]
+        # draw line from starting point up
+        while top_y < genotype.num_of_rows:
+            current_node_val = genotype.grid[layer][top_y][starting_point.x]
 
             # see example for this condition in the article section 4.3, image (a).
-            if abs(current_node_val) > 0 and (abs(current_node_val) != net_number or not initial_line):
+            # initial line don't stop on nodes with the same net number
+            if abs(current_node_val) > 0 and (abs(current_node_val) != net_number and not initial_line):
                 break
-            grid[layer][bottom_y][starting_point.x] = net_number
+            genotype.grid[layer][top_y][starting_point.x] = net_number
             top_y += 1
+        top_y = genotype.num_of_rows if top_y >= genotype.num_of_rows else top_y + 1
 
-        return Point2D(x=starting_point.x, y=randrange(bottom_y + 1, top_y - 1))
+        return Point2D(x=starting_point.x, y=randrange(bottom_y, top_y))
+
+    def copy_path_to_genotype(self, path: List[Point3D], net_num: int):
+
+        for i, point in enumerate(path):
+            # assign negative value to pins (start/end of the path).
+            value = -net_num if i == 0 or i == len(path) - 1 else net_num
+            self.genotype.grid[point.z][point.y][point.x] = net_num
 
     def random_routing(self, pin_a: Pin, pin_b: Pin) -> bool:
         """
         see section 4.3 in the article 3rd paragraph about random routing.
-        :param pin_a: first pin to connect
-        :param pin_b: second pin to connect
-        :return: return True if succeed else False.
+        @param pin_a: first pin to connect
+        @param pin_b: second pin to connect
+        @return: return True if succeed else False.
         """
-
-        # work on a copy instead of the original grid
-        grid_copy = deepcopy(self.genotype.grid)
+        # work on a copy instead of the original genotype
+        genotype_copy = deepcopy(self.genotype)
 
         # see formula 1 in the article, section 4.3 .
-        curr_number_of_rows = len(self.genotype.grid[0])
-        max_num_of_iter = 3 * abs(pin_a.x - pin_b.x) + curr_number_of_rows + 10
+        max_num_of_iter = 3 * abs(pin_a.x - pin_b.x) + genotype_copy.num_of_rows + 10
 
         net_number = abs(pin_a.value)
-        # create initial vertical lines. see image (a) in the article section 4.3
-        pin_a_random_point = self._vertical_line(grid=grid_copy,
-                                                 starting_point=Point2D(x=pin_a.x, y=pin_a.y),
-                                                 initial_line=True,
-                                                 net_number=net_number)
-        pin_b_random_point = self._vertical_line(grid=grid_copy,
-                                                 starting_point=Point2D(x=pin_b.x, y=pin_b.y),
-                                                 initial_line=True,
-                                                 net_number=net_number)
-        i = 0
-        while i < max_num_of_iter:
-
+        # initial points for vertical lines
+        pin_a_random_point = Point2D(x=pin_a.x, y=pin_a.y)
+        pin_b_random_point = Point2D(x=pin_b.x, y=pin_b.y)
+        for i in range(max_num_of_iter):
+            # vertical line from random point
+            pin_a_random_point = self._vertical_line(genotype=genotype_copy, starting_point=pin_a_random_point,
+                                                     initial_line=i == 0, net_number=net_number)
+            pin_b_random_point = self._vertical_line(genotype=genotype_copy, starting_point=pin_b_random_point,
+                                                     initial_line=i == 0, net_number=net_number)
             # horizontal line from random point
-            pin_a_random_point = self._horizontal_line(grid=grid_copy, starting_point=pin_a_random_point,
+            pin_a_random_point = self._horizontal_line(genotype=genotype_copy, starting_point=pin_a_random_point,
                                                        net_number=net_number)
-            pin_b_random_point = self._horizontal_line(grid=grid_copy, starting_point=pin_b_random_point,
+            pin_b_random_point = self._horizontal_line(genotype=genotype_copy, starting_point=pin_b_random_point,
                                                        net_number=net_number)
 
-            # vertical lines from random point
-            pin_a_random_point = self._vertical_line(grid=grid_copy, starting_point=pin_a_random_point,
-                                                     net_number=net_number)
-            pin_b_random_point = self._vertical_line(grid=grid_copy, starting_point=pin_b_random_point,
-                                                     net_number=net_number)
+            path = genotype_copy.find_shortest_path(point1=Point3D(x=pin_a.x, y=pin_a.y, z=pin_a.z),
+                                                    point2=Point3D(x=pin_b.x, y=pin_b.y, z=pin_b.z))
+            if path:
+                self.copy_path_to_genotype(path=path, net_num=net_number)
+                return True
 
-            i += 1
-
-        if i >= max_num_of_iter:
-            return False
-
-        self.genotype.grid = grid_copy
-        return True
+        return False
 
     def mutate(self):
         pass
