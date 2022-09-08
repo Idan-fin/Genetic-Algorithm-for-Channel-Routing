995816f7bc75f7f11fb486dcd42cc077fc4f06bb
yoto5 <yotam.toledano@gmail.com>
2022-09-08 18:23:48 +0300

    - tests for random routing - mock input params - few bugs

---
+++ genetic_algo/test/data/random_routing_test_data.py
@@ -0,0 +1,13 @@
+MOCK_INPUT_DATA = {
+    'pins_position': [[1, 2, 3], [4, 5, 6]],
+    'population_size': 20,
+    'net_length_factor': 0.5,
+    'via_numbers_factor': 0.5,
+    'max_descendants': 20,
+    'max_generations': 20,
+    'mut_1_prob': 0.5,
+    'mut_2_prob': 0.5,
+    'mut_3_prob': 0.5,
+    'mut_4_prob': 0.5,
+    'expected_final_row_num': 15
+}
---
+++ genetic_algo/test/mock_classes/mock_input_params.py
@@ -0,0 +1,20 @@
+from genetic_algo.classes.input_params import InputParams
+
+
+class MockInputParams(InputParams):
+
+    def _parse_input_file(self):
+        pass
+
+    def _parse_parameters_from_kwargs(self, **kwargs):
+        self.pins_position = kwargs['pins_position']
+        self.population_size = kwargs['population_size']
+        self.net_length_factor = kwargs['net_length_factor']
+        self.via_numbers_factor = kwargs['via_numbers_factor']
+        self.max_descendants = kwargs['max_descendants']
+        self.max_generations = kwargs['max_generations']
+        self.mut_1_prob = kwargs['mut_1_prob']
+        self.mut_2_prob = kwargs['mut_2_prob']
+        self.mut_3_prob = kwargs['mut_3_prob']
+        self.mut_4_prob = kwargs['mut_4_prob']
+        self.expected_final_row_num = kwargs['expected_final_row_num']
