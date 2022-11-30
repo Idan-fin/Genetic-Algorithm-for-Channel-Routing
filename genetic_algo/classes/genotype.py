eb6de54eb9a18eafde2a3ecf1b5c812d0635f833
Idan-fin <53435486+Idan-fin@users.noreply.github.com>
2022-12-01 00:48:10 +0200

    add all edges together to the graph

--- genetic_algo/classes/genotype.py
+++ genetic_algo/classes/genotype.py
@@ -20,19 +20,19 @@ class Genotype:
 
     @staticmethod
     def _generate_initial_genotype(num_of_row: int, pins_position: List[List[int]]) -> List[List[List[int]]]:
-        x = num_of_row #random.randint(num_of_row*2, num_of_row*4)
+        x = num_of_row  # random.randint(num_of_row*2, num_of_row*4)
 
         genotype = [[[0 for k in range(len(pins_position[0]))] for j in range(x)] for i in range(2)]
         for j, l in enumerate(genotype[0][0]):
             genotype[0][0][j] = -pins_position[0][j]
-        for j, l in enumerate(genotype[0][x-1]):
-            genotype[0][x-1][j] = -pins_position[1][j]
+        for j, l in enumerate(genotype[0][x - 1]):
+            genotype[0][x - 1][j] = -pins_position[1][j]
         return genotype
 
         pass
 
     def _calculate_edge_index(self, x: int, y: int, z: int) -> int:
-        return len(self.grid)*len(self.grid[0])*z+len(self.grid)*y+x
+        return len(self.grid) * len(self.grid[0]) * z + len(self.grid) * y + x
         pass
 
     def _calculate_edge_from_point(self, p: Point3D) -> int:
@@ -40,31 +40,31 @@ class Genotype:
         pass
 
     def create_graph(self, net_id: int) -> Graph:
+        eslist = []
         g = Genotype.g
         g.clear()
-        g.add_vertices(len(self.grid)*len(self.grid[0])*len(self.grid[0][0]))
+        g.add_vertices(len(self.grid) * len(self.grid[0]) * len(self.grid[0][0]))
         for layer_index, layer in enumerate(self.grid):
             for row_index, row in enumerate(layer):
                 for column_index, val in enumerate(row):
-                    if row_index <= len(self.grid[0])-2 and \
-                            abs(self.grid[layer_index][row_index+1][column_index]) == abs(val) == net_id:
-                        g.add_edge(self._calculate_edge_index(layer_index, row_index, column_index),
-                                   (self._calculate_edge_index(layer_index, row_index+1, column_index)))
-                    if column_index <= len(row)-2 and len(self.grid[0])-1 > row_index > 0 and \
-                            abs(self.grid[layer_index][row_index][column_index+1]) == abs(val) == net_id:
-                        g.add_edge(self._calculate_edge_index(layer_index, row_index, column_index),
-                                   (self._calculate_edge_index(layer_index, row_index, column_index+1)))
-
-                    if layer_index == 0 and 0 < row_index < len(self.grid[0])-1 \
+                    if row_index <= len(self.grid[0]) - 2 and \
+                            abs(self.grid[layer_index][row_index + 1][column_index]) == abs(val) == net_id:
+                        eslist.append((self._calculate_edge_index(layer_index, row_index, column_index),
+                                       (self._calculate_edge_index(layer_index, row_index + 1, column_index))))
+                    if column_index <= len(row) - 2 and len(self.grid[0]) - 1 > row_index > 0 and \
+                            abs(self.grid[layer_index][row_index][column_index + 1]) == abs(val) == net_id:
+                        eslist.append((self._calculate_edge_index(layer_index, row_index, column_index),
+                                       (self._calculate_edge_index(layer_index, row_index, column_index + 1))))
+                    if layer_index == 0 and 0 < row_index < len(self.grid[0]) - 1 \
                             and abs(self.grid[1][row_index][column_index]) == abs(val) == net_id:
-                        g.add_edge(self._calculate_edge_index(0, row_index, column_index),
-                                   (self._calculate_edge_index(1, row_index, column_index)))
-
+                        eslist.append((self._calculate_edge_index(0, row_index, column_index),
+                                   (self._calculate_edge_index(1, row_index, column_index))))
+        g.add_edges(eslist)
         return g
 
     def calculate_genotype_index(self, index: int) -> Point3D:
-        z: int = index // (len(self.grid)*len(self.grid[0]))
-        index -= (z * len(self.grid)*len(self.grid[0]))
+        z: int = index // (len(self.grid) * len(self.grid[0]))
+        index -= (z * len(self.grid) * len(self.grid[0]))
         y: int = index // len(self.grid)
         x: int = index % len(self.grid)
         return Point3D(x, y, z)
@@ -72,7 +72,6 @@ class Genotype:
     def calculate_genotype_index_fixed(self, index: int) -> Point3D:
         point = self.calculate_genotype_index(index=index)
         return Point3D(point.z, point.y, point.x)
-
 
     def find_shortest_path(self, point1: Point3D, point2: Point3D) -> Optional[List[Point3D]]:
         """
--- genetic_algo/main.py
+++ genetic_algo/main.py
@@ -1,9 +1,10 @@
 from genetic_algo.classes.genetic_algorithm import GeneticAlgorithm
 from genetic_algo.classes.input_params import InputParams
 from genetic_algo.classes.output_resolver import OutputResolver
-
+from random import seed
 
 def main():
+    seed(13)
     input_params = InputParams('test4.json')
     genetic_algo = GeneticAlgorithm(input_params=input_params)
     OutputResolver(genetic_algo.run(), "out.json").resolve_output()
