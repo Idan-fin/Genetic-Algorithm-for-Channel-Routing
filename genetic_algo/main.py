363e7b627ecac99fb92c083195b6497b79af71a9
Idan-fin <53435486+Idan-fin@users.noreply.github.com>
2022-12-22 22:21:10 +0200

    added result prints

--- genetic_algo/main.py
+++ genetic_algo/main.py
@@ -4,13 +4,34 @@ from genetic_algo.classes.output_resolver import OutputResolver
 from random import seed
 
 
+def print_colored_2d_list(lst):
+    # define a list of colors to use for the numbers
+    colors = ['\033[1;31m', '\033[1;32m', '\033[1;33m', '\033[1;34m', '\033[1;35m', '\033[1;36m']
+
+    # iterate through the rows of the list
+    for row in lst:
+        # iterate through the numbers in the row
+        for num in row:
+            # choose a color for the number based on its value
+            color = colors[abs(num) % len(colors)]
+            # print the number with the chosen color
+            print(f"{color}{abs(num)}\033[0m", end=' ')
+        # move to the next line after printing the row
+        print()
+
+
 def main():
     # seed(13)
     print("start")
     input_params = InputParams('test4.json')
     genetic_algo = GeneticAlgorithm(input_params=input_params)
-    OutputResolver(genetic_algo.run(), "out.json").resolve_output()
+    result = genetic_algo.run()
+    OutputResolver(result, "out.json").resolve_output()
     print("end")
+    print("layer1:")
+    print_colored_2d_list(result.best_solution.genotype.grid[0])
+    print("layer2:")
+    print_colored_2d_list(result.best_solution.genotype.grid[1])
 
 
 if __name__ == '__main__':
