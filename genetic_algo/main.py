a4fccf195efd3d61a0739031347490536d6cebd6
Idan-fin <53435486+Idan-fin@users.noreply.github.com>
2022-12-24 13:11:30 +0200

    .

--- genetic_algo/main.py
+++ genetic_algo/main.py
@@ -6,16 +6,19 @@ from random import seed
 
 def print_colored_2d_list(lst):
     # define a list of colors to use for the numbers
-    colors = ['\033[1;31m', '\033[1;32m', '\033[1;33m', '\033[1;34m', '\033[1;35m', '\033[1;36m']
+    colors = ['\033[1;31m', '\033[1;32m', '\033[1;33m', '\033[1;34m', '\033[1;35m', '\033[1;36m',
+              '\033[1;37m', '\033[1;38m', '\033[1;39m', '\033[1;40m', '\033[1;41m', '\033[1;42m',
+              '\033[1;43m', '\033[1;44m', '\033[1;45m', '\033[1;46m', '\033[1;47m', '\033[1;48m']
 
     # iterate through the rows of the list
     for row in lst:
         # iterate through the numbers in the row
         for num in row:
+            num = abs(num)
             # choose a color for the number based on its value
-            color = colors[abs(num) % len(colors)]
-            # print the number with the chosen color
-            print(f"{color}{abs(num)}\033[0m", end=' ')
+            color = colors[num % len(colors)]
+            # print the number with the chosen color and fixed-width formatting
+            print(f"{color}{num:>3}\033[0m", end=' ')
         # move to the next line after printing the row
         print()
 
